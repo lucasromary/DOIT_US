8b80fa4856e34c1029330ae8ee6921202400746c
Lucas <lulu.romary@gmail.com>
2022-12-12 21:33:55 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <Ultrasonic.h>
+
+/*
+ * Pass as a parameter the trigger and echo pin, respectively,
+ * or only the signal pin (for sensors 3 pins), like:
+ * Ultrasonic ultrasonic(13);
+ */
+
+//Ultrasonic ultrasonic(4, 2);
+Ultrasonic ultrasonic(GPIO_NUM_18, GPIO_NUM_5);
+
+int distance;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Pass INC as a parameter to get the distance in inches
+  distance = ultrasonic.read();
+  
+  Serial.print("Distance in CM: ");
+  Serial.println(distance);
+  delay(1000);
+}
